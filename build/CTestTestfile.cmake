4cb8bf1a0cd01f0ed6397484fad36e48d156c140
Vaibhav Garg <vaibhav.garg237@gmail.com>
2023-10-15 19:38:15 +0530

    initial client working

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vaibhav/Desktop/attempt4/client
+# Build directory: /home/vaibhav/Desktop/attempt4/client/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
